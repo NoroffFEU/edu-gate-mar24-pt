47f06fd372dd32693464d89152b79f0d83180751
RebeckaKinn <rebeckaki@getacademy.no>
2025-09-08 13:27:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ import { initRouter } from "./router/router.js";
 import Navbar from "./components/navbar.js";
 import Footer from "./components/footer.js";
 
-// Render layout components
 document.getElementById("navbar").innerHTML = Navbar();
 document.getElementById("footer").innerHTML = Footer();
 
-// Initialize router
 initRouter();
