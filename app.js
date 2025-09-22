5f2d2194df9ee0fec8c80a312c8037df25724631
RebeckaKinn <rebeckaki@getacademy.no>
2025-09-22 22:39:31 +0200

    adding css and cuntionality for the header menu
    I added some temporary css for the menu when it is opened on mobile. Got it to work, but need to figure out how it is suppose to look on mobile to finish it, as well as tweak the header.

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ import Footer from "./components/footer.js";
 document.getElementById("header").innerHTML = Header();
 document.getElementById("footer").innerHTML = Footer();
 
+document.addEventListener("click", (e) => {
+  if (e.target.id === "burger-btn") {
+    document.getElementById("nav-links").classList.toggle("show");
+  }
+});
+
 initRouter();
--- components/navbar.js
+++ components/navbar.js
@@ -1,10 +1,11 @@
 export default function Navbar() {
   return /*HTML*/ `
     <nav class="header-navbar" id="header-navbar">
-      <a href="/" data-link class="nav-clicked">Dashboard</a>
-      <a href="/about" data-link>About</a>
-      <a href="/" data-link>Contact</a>
-      ${profileBtnInHeader()}
+      ${burgerBtn()}
+      <div class="nav-links" id="nav-links">
+        ${navigationOptions()}
+        ${profileBtnInHeader()}
+      </div>
     </nav>
   `;
 }
@@ -14,11 +15,28 @@ function profileBtnInHeader(){
      return /*HTML*/`
      <div>
       <img 
-               src="./public/icons/user-circle.png" 
-               alt="" 
-               loading="lazy"
-               class="UserLogo">
+        src="./public/icons/user-circle.png" 
+        alt="" 
+        loading="lazy"
+        class="UserLogo">
     </div>
       
      `;
+}
+
+function navigationOptions(){
+  return /*HTML*/ `
+      <a href="/" data-link class="nav-clicked">Dashboard</a>
+      <a href="/about" data-link>About</a>
+      <a href="/" data-link>Contact</a>
+
+  `;
+}
+
+function burgerBtn(){
+  return /*HTML*/`
+    <button class="burgerMenu" id="burger-btn">
+        open menu
+    </button>
+  `;
 }
