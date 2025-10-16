c2c61fb32f14e0c3a8f122977c5209e9c52c6a8b
Rebecka Kinn <rebeckaki@getacademy.no>
2025-10-16 13:00:54 +0200

    added css to the student dashboard

--- pages/students.js
+++ pages/students.js
@@ -1,8 +1,38 @@
 export default function Students(){
     return /*HTML*/`
-    <div>
-    <h1>Testing student page</h1>
-        <a href="/" data-link>Home</a>
+    <div class="student-dashboard">
+        <h1>Dashboard</h1>
+        <div class="student-dashboard-information">
+            <div class="student-profile-logo">
+                <img src="public/icons/profile-icon.png" alt="" loading="lazy"/>
+            </div>
+            <h2>Joe Bloggs</h2>
+            <h3>Student at Edugate school</h3>
+        </div>
+
+        <section class="student-dashboard-buttons">
+            <a href="/" data-link>
+                <div>
+                    <img src="public/icons/user-circle.png" alt="" loading="lazy"/>
+                </div>
+                <p>See profile</p>
+            </a>
+            <a href="/" data-link>
+                <div>
+                    <img src="public/icons/results.png" alt="" loading="lazy"/>
+                </div>
+                <p>My results</p>
+            </a>
+            <a href="/" data-link>
+                <div>
+                    <img src="public/icons/logout.png" alt="" loading="lazy"/>
+                </div>
+                <p>Log out</p>
+            </a>
+        </section>
     </div>  
     `;
 }
+
+//add correct href paths
+//change data to using the json data?
